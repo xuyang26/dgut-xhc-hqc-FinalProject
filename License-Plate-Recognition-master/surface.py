7b72d935dd86a3b1d58a3bd17184e6f96d653e09
Xuyang26 <1076619891@qq.com>
2021-12-28 14:36:15 +0800

    界面布局修改

--- License-Plate-Recognition-master/surface.py
+++ License-Plate-Recognition-master/surface.py
@@ -17,24 +17,23 @@ class Surface(ttk.Frame):
 	thread = None
 	thread_run = False
 	camera = None
-	color_transform = {"green":("绿牌","#55FF55"), "yello":("黄牌","#FFFF00"), "blue":("蓝牌","#6666FF")}
+	color_transform = {"green":("绿牌","#80ff55"), "yello":("黄牌","#ffd455"), "blue":("蓝牌","#6680ff")}
 		
 	def __init__(self, win):
 		ttk.Frame.__init__(self, win)
 		frame_left = ttk.Frame(self)
 		frame_right1 = ttk.Frame(self)
 		frame_right2 = ttk.Frame(self)
-		win.title("车牌识别")
+		win.title("xhc-hqc-数字图像处理大作业-dgut-车牌识别")
 		win.state("zoomed")
 		self.pack(fill=tk.BOTH, expand=tk.YES, padx="5", pady="5")
 		frame_left.pack(side=LEFT,expand=1,fill=BOTH)
 		frame_right1.pack(side=TOP,expand=1,fill=tk.Y)
-		frame_right2.pack(side=RIGHT,expand=0)
+		frame_right2.pack(side=TOP,expand=0)
 		ttk.Label(frame_left, text='原图：').pack(anchor="nw") 
 		ttk.Label(frame_right1, text='车牌位置：').grid(column=0, row=0, sticky=tk.W)
 		
 		from_pic_ctl = ttk.Button(frame_right2, text="来自图片", width=20, command=self.from_pic)
-		from_vedio_ctl = ttk.Button(frame_right2, text="来自摄像头", width=20, command=self.from_vedio)
 		self.image_ctl = ttk.Label(frame_left)
 		self.image_ctl.pack(anchor="nw")
 		
@@ -45,7 +44,7 @@ class Surface(ttk.Frame):
 		self.r_ctl.grid(column=0, row=3, sticky=tk.W)
 		self.color_ctl = ttk.Label(frame_right1, text="", width="20")
 		self.color_ctl.grid(column=0, row=4, sticky=tk.W)
-		from_vedio_ctl.pack(anchor="se", pady="5")
+
 		from_pic_ctl.pack(anchor="se", pady="5")
 		self.predictor = predict.CardPredictor()
 		self.predictor.train_svm()
@@ -87,20 +86,7 @@ class Surface(ttk.Frame):
 			self.r_ctl.configure(text="")
 			self.color_ctl.configure(state='disabled')
 		
-	def from_vedio(self):
-		if self.thread_run:
-			return
-		if self.camera is None:
-			self.camera = cv2.VideoCapture(0)
-			if not self.camera.isOpened():
-				mBox.showwarning('警告', '摄像头打开失败！')
-				self.camera = None
-				return
-		self.thread = threading.Thread(target=self.vedio_thread, args=(self,))
-		self.thread.setDaemon(True)
-		self.thread.start()
-		self.thread_run = True
-		
+
 	def from_pic(self):
 		self.thread_run = False
 		self.pic_path = askopenfilename(title="选择识别图片", filetypes=[("jpg图片", "*.jpg")])
@@ -132,7 +118,7 @@ class Surface(ttk.Frame):
 				r, roi, color = self.predictor.predict(img_bgr)
 				self.show_roi(r, roi, color)
 				predict_time = time.time()
-		print("run end")
+		print("end")
 		
 		
 def close_window():
